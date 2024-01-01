4d7ef6ddb9612148754df5ef1268527ace286947
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-01 17:42:18 +0530

    load the data

---
+++ scripts/time_series_simple_exponential_smoothing.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+from statistics import mean 
+
+import warnings
+warnings.filterwarnings("ignore")
+
+train_data = pd.read_csv("/Users/paramanandbhat/Downloads/5_-_exponential_smoothing_models/data/train_data.csv")
+valid_data = pd.read_csv("/Users/paramanandbhat/Downloads/5_-_exponential_smoothing_models/data/valid_data.csv")
+
+print(train_data.shape)
+print(train_data.head())
